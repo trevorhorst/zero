29367edc2cd6553521ccee6a88fffd363e71e37d
Trevor Horst <trevorhorst1212@gmail.com>
2022-09-07 22:59:02 -0400

    Removing obsolete canvas methods. Rotation now accounts for sprite magnification.

--- common/src/draw/canvas.cpp
+++ common/src/draw/canvas.cpp
@@ -63,6 +63,12 @@ void canvas_print(Canvas *canvas)
 
 void canvas_set_pixel(Canvas *canvas, uint32_t x_point, uint32_t y_point, uint32_t rotate, uint32_t mirror, CanvasColor color)
 {
+    if(x_point >= canvas->width) {
+        return;
+    } else if(y_point >= canvas->height) {
+        return;
+    }
+
     uint32_t x = x_point;
     uint32_t y = y_point;    
 
@@ -109,55 +115,6 @@ void canvas_set_pixel(Canvas *canvas, uint32_t x_point, uint32_t y_point, uint32
     }
 }
 
-void canvas_set_sprite_pixel(Canvas *canvas, BmpSprite *sprite, uint32_t x_point, uint32_t y_point, uint32_t rotate, uint32_t mirror, CanvasColor color)
-{
-    uint32_t x = x_point;
-    uint32_t y = y_point;    
-
-    // Rotates in degress to the right
-    switch(rotate) {
-    case 0:
-        x = x_point;
-        y = y_point;  
-        break;
-    case 90:
-        x = y_point;
-        y = sprite->height - x_point - 1;
-        break;
-    case 180:
-        x = sprite->width - x_point - 1;
-        y = sprite->height - y_point - 1;
-        break;
-    case 270:
-        x = sprite->width - y_point - 1;
-        y = x_point;
-        break;
-    default:
-        return;
-    }
-
-    switch(mirror) {
-    case CANVAS_MIRROR_NONE:
-        break;
-    case CANVAS_MIRROR_HORIZONTAL:
-        x = sprite->width - x - 1;
-        break;
-    case CANVAS_MIRROR_VERTICAL:
-        y = sprite->height - y - 1;
-        break;
-    default:
-        break;
-    }
-
-    uint32_t canvas_x = x / 8;
-    uint32_t canvas_y = y * (canvas->width / 8);
-    if(color == CanvasColor::BLACK) {
-        canvas->image[canvas_x + canvas_y] &= ~(0x80 >> (x % 8));
-    } else {
-        canvas->image[canvas_x + canvas_y] |= (0x80 >> (x % 8));
-    }
-}
-
 void canvas_draw_point(Canvas *canvas, uint32_t x_point, uint32_t y_point, 
                        CanvasColor color, uint8_t size)
 {
@@ -171,26 +128,12 @@ void canvas_draw_point(Canvas *canvas, uint32_t x_point, uint32_t y_point,
     }
 }
 
-void canvas_draw_sprite_point(Canvas *canvas, BmpSprite *sprite, uint32_t x_point, uint32_t y_point, 
-                       CanvasColor color, uint8_t size)
-{
-    uint32_t x_coordinate = 0;
-    uint32_t y_coordinate = 0;
-    for(x_coordinate = 0; x_coordinate < size; x_coordinate++) {
-        for(y_coordinate = 0; y_coordinate < size; y_coordinate++) {
-            canvas_set_sprite_pixel(canvas, sprite, (x_point + x_coordinate), 
-                             (y_point + y_coordinate), 0, CANVAS_MIRROR_NONE, color);
-        }
-    }
-}
-
 void canvas_draw_line(Canvas *canvas, uint32_t x_start, uint32_t y_start, 
                       uint32_t x_end, uint32_t y_end)
 {
     int dx = (int)x_end - (int)x_start >= 0 ? x_end - x_start : x_start - x_end;
     int dy = (int)y_end - (int)y_start <= 0 ? y_end - y_start : y_start - y_end;
 }
-
 
 void canvas_draw_bmp_sprite(Canvas *canvas, Bitmap *bmp, BmpSprite *sprite,
                             uint32_t offset_x, uint32_t offset_y)
@@ -230,16 +173,16 @@ void canvas_draw_bmp_sprite(Canvas *canvas, Bitmap *bmp, BmpSprite *sprite,
                 uint32_t y_point = 0;
                 switch(sprite->rotate) {
                 case(CANVAS_ROTATE_270):
-                    x_point = (sprite->width - canvas_y_point) - 1;
+                    x_point = (sprite->width * size - canvas_y_point) - 1;
                     y_point = canvas_x_point;
                     break;
                 case(CANVAS_ROTATE_180):
-                    x_point = (sprite->width - canvas_x_point) - 1;
-                    y_point = (sprite->height - canvas_y_point) - 1;
+                    x_point = (sprite->width * size - canvas_x_point) - 1;
+                    y_point = (sprite->height * size - canvas_y_point) - 1;
                     break;
                 case(CANVAS_ROTATE_90):
                     x_point = canvas_y_point;
-                    y_point = (sprite->height - canvas_x_point) - 1;
+                    y_point = (sprite->height * size - canvas_x_point) - 1;
                     break;
                 default:
                     x_point = canvas_x_point;
@@ -261,26 +204,6 @@ void canvas_draw_bmp_sprite(Canvas *canvas, Bitmap *bmp, BmpSprite *sprite,
                     canvas_draw_point(canvas, x_point, y_point, black, size);
                 }
             }
-        }
-    }
-}
-
-void canvas_byte_flip(Canvas *canvas)
-{
-    int32_t x = 0;
-    int32_t y = 0;
-    int32_t bit = 0;
-    for(y = 0; y < canvas->height; y++) {
-        for(x = 0; x < (canvas->width / 8); x++) {
-            uint8_t byte = canvas->image[x + (y * (canvas->width / 8))];
-            canvas->image[x + (y * (canvas->width / 8))] = 
-                (canvas_reverse_nibble_lut[byte & 0xF] << 4) |
-                (canvas_reverse_nibble_lut[byte >> 4]);
-            // for(bit = 0; bit < 8; bit++) {
-            //     if(byte & (0x1 << bit)) {
-            //         canvas->image[x + (y * (canvas->width / 8))] |= (0x80 >> bit);
-            //     }
-            // }
         }
     }
 }
