868059f5817b774228b92127c0ac78c6c8c17bdf
Trevor Horst <trevorhorst1212@gmail.com>
2022-09-09 09:57:37 -0400

    [OLED] Project will now render grayscale pokemon images to screen.

--- project/oled/src/application.cpp
+++ project/oled/src/application.cpp
@@ -38,7 +38,7 @@ int32_t application_run()
     sleep_ms(1000);
 
     LOG_INFO("Initializing I2C...\n");
-    i2c_init(i2c1, I2C_BUS_SPEED_KHZ(2000));
+    i2c_init(i2c1, I2C_BUS_SPEED_KHZ(1000));
     gpio_set_function(PIN_I2C1_SDA, GPIO_FUNC_I2C);
     gpio_set_function(PIN_I2C1_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(PIN_I2C1_SDA);
@@ -53,20 +53,23 @@ int32_t application_run()
     BmpSpriteSheet ss_font;
     bmpss_initialize(&ss_font, red_blue_font_bmp, red_blue_font_bmp_size);
 
-    Canvas frambuffer[3];
-
     BmpSprite sprite;
     sprite.height = SPRITE_HEIGHT;
     sprite.width = SPRITE_WIDTH;
-    sprite.x = 0;
-    sprite.y = 0;
     sprite.invert = 0;
+    sprite.magnify = 1;
+    sprite.rotate = CANVAS_ROTATE_270;
+
+    index_to_sprite(123, &ss_grayscale, &sprite);
+
+
+    // bmpss_print_grayscale_sprite(&ss_grayscale, &sprite);
 
     BmpSprite font_sprite;
     font_sprite.height = 8;
     font_sprite.width = 8;
-    font_sprite.x = 0;
-    font_sprite.y = 0;
+    font_sprite.x = 56;
+    font_sprite.y = 56;
     font_sprite.invert = 0;
     font_sprite.rotate = CANVAS_ROTATE_270;
     font_sprite.magnify = 1;
@@ -88,6 +91,23 @@ int32_t application_run()
     uint32_t image_width_bytes  = OLED_HEIGHT;
     uint32_t image_height_bytes = (OLED_WIDTH % 8 == 0) ? (OLED_WIDTH / 8) : ((OLED_WIDTH / 8) + 1);
 
+    uint32_t gs_buffer_length = (image_height_bytes * image_width_bytes) + 1;
+    uint8_t *gs_buffer[3] = {NULL, NULL, NULL};
+    Canvas framebuffer[3];
+
+    LOG_INFO("Testing grayscale BMP sprite...\n");
+    bmpss_print_grayscale_sprite(&ss_grayscale, &sprite);
+    for(uint32_t i = 0; i < 3; i++) {
+        gs_buffer[i] = (uint8_t*)malloc(gs_buffer_length);
+        framebuffer[i].mirror = CANVAS_MIRROR_NONE;
+        framebuffer[i].rotate = CANVAS_ROTATE_0;
+        framebuffer[i].height = OLED_WIDTH;
+        framebuffer[i].width  = OLED_HEIGHT;
+        framebuffer[i].image = &gs_buffer[i][1];
+        canvas_fill(&framebuffer[i], 0x00);
+        // canvas_draw_grayscale_bmp_sprite(&framebuffer[i], &(ss_grayscale.bitmap), &sprite, i, offset_x, offset_y);
+    }
+
     uint32_t buffer_length = (image_height_bytes * image_width_bytes) + 1;
     uint8_t *buffer = (uint8_t*)malloc(buffer_length);
 
@@ -102,15 +122,20 @@ int32_t application_run()
     canvas_fill(&canvas, 0xFF);
 
     ssd1306_write_buffer(&dev, buffer, buffer_length);
+    ssd1306_reset_cursor(&dev);
 
     LOG_INFO("   OLED Version: %s\n", OLED_VERSION);
     LOG_INFO(" Common Version: %s\n", COMMON_VERSION);
 
     uint32_t dexNumber = 1;
+    uint32_t framestart = 0;
+    uint32_t frameend = 0;
     do {
-        uint64_t start = to_us_since_boot(get_absolute_time());
-        // Reset the cursor to the starting point
-        ssd1306_reset_cursor(&dev);
+
+
+        // uint64_t start = to_us_since_boot(get_absolute_time());
+        // // Reset the cursor to the starting point
+        // ssd1306_reset_cursor(&dev);
         
         if((dexNumber < 1) || (dexNumber > 151)) {
             // Reset the dex counter if we go out of bounds
@@ -118,27 +143,39 @@ int32_t application_run()
         } 
 
         // Select the pokemon sprite
-        sprite.height = SPRITE_HEIGHT;
-        sprite.width = SPRITE_WIDTH;
-        sprite.magnify = poke_sprite_magnify;
-        sprite.invert = 0;
-        sprite.rotate = CANVAS_ROTATE_270;
         index_to_sprite(dexNumber, &ss, &sprite);
-        index_to_sprite(1, &ss_font, &font_sprite);
+        // index_to_sprite(1, &ss_font, &font_sprite);
 
-        // Draw the sprite to the canvas and flip bytes
-        canvas_draw_bmp_sprite(&canvas, &(ss.bitmap), &sprite, offset_x, offset_y);
-        // canvas_draw_bmp_sprite(&canvas, &(ss_font.bitmap), &font_sprite, offset_x, offset_y);
+        for(uint32_t i = 0; i < 3; i++) {
+            canvas_draw_grayscale_bmp_sprite(&framebuffer[i], &(ss_grayscale.bitmap), &sprite, i, offset_x, offset_y);
+        }
 
-        // Draw to screen
-        ssd1306_write_buffer(&dev, buffer, buffer_length);
-        uint64_t end = to_us_since_boot(get_absolute_time());
-        LOG_INFO("Render time: %llu\n", end - start);
+        framestart = to_ms_since_boot(get_absolute_time());
+        frameend = framestart;
+        while((frameend - framestart) < 1000) {
+            for(uint32_t i = 0; i < 3; i++) {
+                // We shouldn't have to reset the cursor if the entire screen is
+                // being written, the display will automaatically wraparound
+                // ssd1306_reset_cursor(&dev);
+                ssd1306_write_buffer(&dev, gs_buffer[i], gs_buffer_length);
+            }
+            frameend = to_ms_since_boot(get_absolute_time());
+        }
+
+        // // Draw the sprite to the canvas and flip bytes
+        // canvas_draw_bmp_sprite(&canvas, &(ss.bitmap), &sprite, offset_x, offset_y);
+        // // canvas_draw_bmp_sprite(&canvas, &(ss_font.bitmap), &font_sprite, offset_x, offset_y);
+
+        // // Draw to screen
+        // ssd1306_write_buffer(&dev, buffer, buffer_length);
+        // uint64_t end = to_us_since_boot(get_absolute_time());
+        // LOG_INFO("Render time: %llu\n", end - start);
 
         dexNumber++;
 
-        sleep_ms(1000);
     } while(true);
+
+    free(buffer);
 
     return success;
 }
