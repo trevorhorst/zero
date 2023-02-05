a2c66d1007ababed4a5fd41064511918d9882b84
Trevor Horst <trevorhorst1212@gmail.com>
2023-02-05 11:43:57 -0500

    Dewpoint project will now make use of the temperature/humidty sensor driver.

--- project/dewpoint/src/application.c
+++ project/dewpoint/src/application.c
@@ -5,6 +5,7 @@
 #include "hardware/spi.h"
 
 #include "core/console/console.h"
+#include "core/drivers/sht3xdis.h"
 #include "core/drivers/ssd1306.h"
 #include "core/logger.h"
 
@@ -24,11 +25,13 @@
 
 #define BUS_SPEED_KHZ(x) x * 1000
 
+#define CMD_I2C     "i2c"
+
 const char empty_line[]                 = "                ";
 const char temp_line[]                  = "Temp    : %.2f ";
 const char relative_humidity_line[]     = "RelH    : %.2f ";
-const char dewpoint_line[]              = "Dewpoint: %.2f ";
-const char frostpoint_line[]            = "Frost   : %.2f ";
+const char dewpoint_line[]              = "Dew P.  : %.2f ";
+const char frostpoint_line[]            = "Frost P.: %.2f ";
 
 bool i2c_reserved_addr(uint8_t addr)
 {   
@@ -101,6 +104,8 @@ void initialize_spi(spi_inst_t *bus)
 
 void initialize_display(ssd1306_spi_device *display)
 {
+    LOG_INFO("Initializing Display...\n");
+
     // Initialize DC pin
     gpio_init(PIN_DISPLAY_DC);
     gpio_set_dir(PIN_DISPLAY_DC, GPIO_OUT);
@@ -121,20 +126,14 @@ void initialize_display(ssd1306_spi_device *display)
     display->dc    = PIN_DISPLAY_DC;
     display->reset = PIN_DISPLAY_RES;
 
-    LOG_INFO("Initializing Display...\n");
     ssd1306_initialize_device(display);
 }
 
-float temperature_in_c(uint16_t temp)
+void initialize_ht_sensor(sht3xdis_i2c_device *sensor)
 {
-    float temperature = -45.0 + (175. * ((float)temp / 65535.));
-    return temperature;
-}
-
-float temperature_in_f(uint16_t temp)
-{
-    float temperature = -49.0 + (315. * ((float)temp / 65535.));
-    return temperature;
+    LOG_INFO("Initializing Humidity and Temperature Sensor...\n");
+    sensor->address = 0x44;
+    sensor->bus     = i2c0;
 }
 
 float c_to_f(float temp)
@@ -181,6 +180,13 @@ void write_string_to_display(ssd1306_spi_device *display, const char *str )
     }
 }
 
+uint32_t test_i2c(int32_t argc, char **argv)
+{
+    uint32_t error = 0;
+    i2c_bus_scan(i2c0);
+    return error;
+}
+
 int32_t application_run()
 {
     int32_t success = 0;
@@ -189,6 +195,7 @@ int32_t application_run()
     sleep_ms(1000);
 
     ssd1306_spi_device display;
+    sht3xdis_i2c_device ht_sensor = {i2c0, 0x44};
 
     initialize_i2c(i2c0);
     initialize_spi(spi0);
@@ -197,6 +204,10 @@ int32_t application_run()
     LOG_INFO("Dewpoint Version: %s\n", DEWPOINT_VERSION);
     LOG_INFO("  Common Version: %s\n", CORE_VERSION);
 
+
+    struct console_command cmd_i2c  = {&test_i2c};
+    console_initialize();
+    console_add_command(CMD_I2C, &cmd_i2c);
     // Launch the console on the second core
     multicore_launch_core1(&console_run);
 
@@ -207,6 +218,7 @@ int32_t application_run()
     char temperature_text[17];
     char relative_humidity_text[17];
     char dewpoint_text[17];
+    char frostpoint_text[17];
 
     // Run the main thread
     while(true) {
@@ -214,27 +226,24 @@ int32_t application_run()
 
         memset(temperature_text, 0x20, 17);
 
-        uint8_t command[] = {0x2C, 0x06};
-        i2c_write_blocking(i2c0, 0x44, command, 2, false);
-        sleep_ms(1000);
-        uint8_t response[6];
-        i2c_read_blocking(i2c0, 0x44, response, 6, false);
+        sht3xdis_measurement measurement = {0x00, 0x00};
+        measurement = sht3xdis_singleshot_measurement(&ht_sensor, SHT3XDIS_REPEATABILITY_HIGH, true);
 
-        int16_t rawtemp = (response[0] << 8) | response[1];
-        int16_t rawrh   = (response[3] << 8) | response[4];
-
-        float temp_c = temperature_in_c(rawtemp);
-        float temp_f = temperature_in_f(rawtemp);
-        float rh     = relative_humidity(rawrh);
+        float temp_c = sht3xdis_convert_raw_to_celsius(measurement.raw_temperature);
+        float temp_f = sht3xdis_convert_raw_to_farenheit(measurement.raw_temperature);
+        float rh     = relative_humidity(measurement.raw_relative_humidity);
         float dp     = dewpoint_in_c(temp_c, rh);
+        float fp     = calculate_frostpoint_in_c(temp_c, dp);
 
         sprintf(temperature_text, temp_line, temp_f);
         sprintf(relative_humidity_text, relative_humidity_line, rh);
         sprintf(dewpoint_text, dewpoint_line, c_to_f(dp));
+        sprintf(frostpoint_text, frostpoint_line, c_to_f(fp));
 
         write_string_to_display(&display, temperature_text);
         write_string_to_display(&display, relative_humidity_text);
         write_string_to_display(&display, dewpoint_text);
+        write_string_to_display(&display, frostpoint_text);
 
         // printf("Read Temp %fC (%fF)  (0x%02X 0x%02X 0x%02X)\n", temp_c, temp_f, response[0], response[1], response[2]);
         // printf("Read RelH %f  (0x%02X 0x%02X 0x%02X)\n", rh, response[3], response[4], response[5]);
