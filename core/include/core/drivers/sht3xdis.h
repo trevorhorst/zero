c3d1ccf5cd81847903c5dd72cc7bade37e6538a8
Trevor Horst <trevorhorst1212@gmail.com>
2023-02-05 11:40:26 -0500

    Adding a driver for the temperature and humidty sensor.

---
+++ core/include/core/drivers/sht3xdis.h
@@ -0,0 +1,34 @@
+#ifndef SHT3XDIS_H
+#define SHT3XDIS_H
+
+#include "hardware/i2c.h"
+
+// ADDR (pin 2) connected to ground
+#define SHT3XDIX_I2C_ADDRESS_A  0x44
+// ADDR (pin 2) connected to VDD
+#define SHT3XDIX_I2C_ADDRESS_B  0x45
+
+typedef struct sht3xdis_i2c_device_t {
+    i2c_inst_t *bus;
+    uint8_t address;
+} sht3xdis_i2c_device;
+
+typedef struct sht3xdis_measurement_t {
+    int16_t raw_temperature;
+    int16_t raw_relative_humidity;
+} sht3xdis_measurement;
+
+enum sht3xdis_repeatability {
+    SHT3XDIS_REPEATABILITY_HIGH     = 0,
+    SHT3XDIS_REPEATABILITY_MEDIUM   = 1,
+    SHT3XDIS_REPEATABILITY_LOW      = 2
+};
+
+void sht3xdis_write(sht3xdis_i2c_device *device, uint8_t *buffer, uint32_t buffer_length);
+void sht3xdis_read(sht3xdis_i2c_device *device, uint8_t *buffer, uint32_t buffer_length);
+float sht3xdis_convert_raw_to_celsius(uint16_t temp);
+float sht3xdis_convert_raw_to_farenheit(uint16_t temp);
+sht3xdis_measurement sht3xdis_singleshot_measurement(sht3xdis_i2c_device *device, 
+    enum sht3xdis_repeatability repeat, bool clock_stretch);
+
+#endif // SHT3XDIS_H
---
+++ core/src/drivers/sht3xdis.c
@@ -0,0 +1,83 @@
+#include "core/drivers/sht3xdis.h"
+
+/**
+ * @brief Calculate the CRC-8 checksum a received data from the chip
+ * 
+ * @param bytes 
+ * @param nbytes 
+ * @param polynomial 
+ * @return uint32_t 
+ */
+uint32_t calculate_checksum(uint8_t *bytes, uint32_t nbytes)
+{
+    // Polynomial used by the sensor (x^8 + x^5 + x^4 + x^1)
+    uint32_t polynomial = 0x131;
+
+    // Initialize CRC register to 0
+    uint32_t crc = 0;
+
+    for(uint32_t byte = 0; byte < nbytes; byte++)
+    {
+        crc ^= bytes[byte];
+        for(uint32_t bit = 8; bit > 0; --bit) {
+            if(crc & 0x80) {
+                crc = (crc << 1) ^ polynomial;
+            } else { 
+                crc = (crc << 1); 
+            }
+        }
+    }
+
+    return crc;
+}
+
+
+void sht3xdis_write(sht3xdis_i2c_device *device, uint8_t *buffer, uint32_t buffer_length)
+{
+    i2c_write_blocking(device->bus, device->address, buffer, buffer_length, false);
+}
+
+void sht3xdis_read(sht3xdis_i2c_device *device, uint8_t *buffer, uint32_t buffer_length)
+{
+    i2c_read_blocking(device->bus, device->address, buffer, buffer_length, false);
+}
+
+float sht3xdis_convert_raw_to_celsius(uint16_t temp)
+{
+    float temperature = -45. + (175. * ((float)temp / 65535.));
+    return temperature;
+}
+
+float sht3xdis_convert_raw_to_farenheit(uint16_t temp)
+{
+    float temperature = -49. + (315. * ((float)temp / 65535.));
+    return temperature;
+}
+
+sht3xdis_measurement sht3xdis_singleshot_measurement(sht3xdis_i2c_device *device, 
+    enum sht3xdis_repeatability repeat, bool clock_stretch)
+{
+    uint8_t command[2] = {0x00, 0x00};
+    command[0] = clock_stretch ? 0x2C : 0x24;
+    switch(repeat) {
+        case(SHT3XDIS_REPEATABILITY_HIGH):
+            command[1] = clock_stretch ? 0x06 : 0x00;
+            break;
+        case(SHT3XDIS_REPEATABILITY_MEDIUM):
+            command[1] = clock_stretch ? 0x0D : 0x0B;
+            break;
+        case(SHT3XDIS_REPEATABILITY_LOW):
+            command[1] = clock_stretch ? 0x10 : 0x16;
+            break;
+    }
+    sht3xdis_write(device, command, sizeof(command));
+    sleep_ms(1000);
+    uint8_t response[6];
+    sht3xdis_read(device, response, sizeof(response));
+
+    sht3xdis_measurement measurement;
+    measurement.raw_temperature         = (response[0] << 8) | response[1];
+    measurement.raw_relative_humidity   = (response[3] << 8) | response[4];
+
+    return measurement;
+}
